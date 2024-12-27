770942e07d5d2f7bbf26ba40f6f6c33c044df90c
CYP3RBOT <cyp3rbot8009@gmail.com>
2024-12-27 10:38:43 -0500

    Build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-import Pagination from "./components/Pagination";
+import LatestIssue from "./LatestIssue";
 
-const Home = async ({
-  searchParams: rawSearchParams,
-}: {
-  searchParams: Promise<{ page: string }>;
-}) => {
-  const searchParams = await rawSearchParams;
-
-  return (
-    <div>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
-    </div>
-  );
+const Home = () => {
+  return <LatestIssue />;
 };
 
 export default Home;
