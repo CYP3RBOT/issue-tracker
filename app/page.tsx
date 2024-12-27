8e4929b8e1bd79b6e153e7e549538f1da112576c
CYP3RBOT <cyp3rbot8009@gmail.com>
2024-12-27 09:58:09 -0500

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Pagination from "./components/Pagination";
 
-const Home = () => {
+const Home = async ({
+  searchParams: rawSearchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const searchParams = await rawSearchParams;
+
   return (
     <div>
-      <Pagination itemCount={100} pageSize={10} currentPage={10} />
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </div>
   );
 };
